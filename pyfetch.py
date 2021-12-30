534638ce8ff8fe4824091a83dc69c06b0543822a
KotonBads <kotonbads@duck.com>
2021-12-30 23:46:35 +0800

    eliminate the use of grep

--- pyfetch.py
+++ pyfetch.py
@@ -23,25 +23,39 @@ def mem():
     # Formula: usedmem = MemTotal + Shmem - MemFree - Buffers - Cached - SReclaimable
     # Source: https://github.com/KittyKatt/screenFetch/issues/386#issuecomment-249312716
 
-    current_mem = os.popen('cat /proc/meminfo | grep -E "MemTotal|Shmem|MemFree|Buffers|Cached|SReclaimable"').read().strip().split('\n')
-    mem_total = int(current_mem[0].split()[1])
-    mem_free = int(current_mem[1].split()[1])
-    mem_buffers = int(current_mem[2].split()[1])
-    mem_cached = int(current_mem[3].split()[1])
-    mem_shared = int(current_mem[5].split()[1])
-    mem_sreclaimable = int(current_mem[6].split()[1])
-    mem_stats = {
-        'total': mem_total,
-        'free': mem_free,
-        'buffers': mem_buffers,
-        'cached': mem_cached,
-        'sreclaimable': mem_sreclaimable,
-        'shared': mem_shared
-    }
-    return {'used_mem': mem_stats['total'] + mem_stats['shared'] - mem_stats['free'] - mem_stats['buffers'] - mem_stats['cached'] - mem_stats['sreclaimable'], 'total_mem': mem_stats['total']}
+    # current_mem = os.popen('cat /proc/meminfo | grep -E "MemTotal|Shmem|MemFree|Buffers|Cached|SReclaimable"').read().strip().split('\n')
+    
+    with open('/proc/meminfo') as f:
+        current_mem = f.read().strip().split('\n')
+
+        mem_total = int([i for i in current_mem if 'MemTotal' in i][0].split()[1])
+        mem_shared = int([i for i in current_mem if 'Shmem' in i][0].split()[1])
+        mem_free = int([i for i in current_mem if 'MemFree' in i][0].split()[1])
+        mem_buffers = int([i for i in current_mem if 'Buffers' in i][0].split()[1])
+        mem_cached = int([i for i in current_mem if 'Cached' in i][0].split()[1])
+        mem_sreclaimable = int([i for i in current_mem if 'SReclaimable' in i][0].split()[1])
+
+        mem_stats = {
+            'total': mem_total,
+            'free': mem_free,
+            'buffers': mem_buffers,
+            'cached': mem_cached,
+            'sreclaimable': mem_sreclaimable,
+            'shared': mem_shared
+        }
+        return {'used_mem': mem_stats['total'] + 
+            mem_stats['shared'] - mem_stats['free'] - 
+            mem_stats['buffers'] - mem_stats['cached'] - 
+            mem_stats['sreclaimable'], 
+            'total_mem': mem_stats['total']
+            }
 
 ### CPU ###
-cpu = os.popen('lscpu | grep "Model name"').read().split(':')[1].strip()
+def cpu():
+    current_cpu = os.popen('lscpu').read().strip().split('\n')
+    for line in current_cpu:
+        if 'Model name' in line:
+            return line.split(':')[1].strip()
 
 ### KERNEL ###
 kernel = os.popen('uname -r').read().strip()
@@ -58,7 +72,7 @@ free_mem = total_mem - used_mem
 ### OUTPUT STRING ###
 fetch = f"""
 {yellow}  {distro().strip()}
-{red}  {cpu}
+{red}  {cpu()}
 {green}塞 {used_mem}MB / {total_mem}MB
 {blue}  {kernel}
 {cyan}  {shell}
